d2fe7b1c5f0edf0412c3f32a9e7a928316add2c2
Soma Szelpal <szelpalsoma@gmail.com>
2017-06-15 15:40:37 +0200

    Added unit test skeleton

---
+++ tests/CorrelateProcessorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ProEmergotech\Correlate\Monolog\Tests;
+
+use PHPUnit\Framework\TestCase;
+use ProEmergotech\Correlate\Monolog\CorrelateProcessor;
+
+/**
+ *
+ * @category  \ProEmergotech\Correlate\Monolog\Tests
+ * @package   \ProEmergotech\Correlate\Monolog\Tests
+ */
+class CorrelateProcessorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @before
+     */
+    public function setup()
+    {
+
+    }
+}
